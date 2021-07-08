14749bdf0abc4ed29bff60a0dc58b776ec826bea
KaloyanVD <kaloyan.vasilov@abv.bg>
2021-07-08 23:14:26 +0300

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+function Nav() {
+  const [navbarBlack, setNavbarBlack] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setNavbarBlack(true);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <nav className={`${navbarBlack && "nav__black"}`}>
+      <div className="nav__contents">
+        {/* <img
+          className="nav__logo"
+          src="https://i.pinimg.com/564x/1b/54/ef/1b54efef3720f6ac39647fc420d4a6f9.jpg"
+        /> */}
+        <img
+          className="nav__avatar"
+          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
